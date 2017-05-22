1a91f4212ddbdaad084aebcc5cd2fdf43e40edde
HeberSobreira <heber.sobreira@gmail.com>
2017-05-22 14:26:05 +0100

    correcceo de bug

--- example/serial_communication_example/edgar_arm_communications.c
+++ example/serial_communication_example/edgar_arm_communications.c
@@ -4,9 +4,9 @@ TDataCommunicationsChannels DataCommunicationsChannels_;
 
 TDataCommunicationsChannels *InitEdgarArmCommunications()
 {
-    AddVallueToChannel_int32_t('g', (int32_t *)&(DataCommunicationsChannels_.dataTest1));
-    AddVallueToChannel_int64_t('r', &(DataCommunicationsChannels_.dataTest2));
-    AddVallueToChannel_float('r', &(DataCommunicationsChannels_.dataTest3));
+    AddValueToChannel_int32_t('g', &(DataCommunicationsChannels_.dataTest1));
+    AddValueToChannel_int64_t('r', &(DataCommunicationsChannels_.dataTest2));
+    AddValueToChannel_float('r', &(DataCommunicationsChannels_.dataTest3));
 
     return &DataCommunicationsChannels_;
 }
